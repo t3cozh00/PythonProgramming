ae0a4d9dba237d86d057111bea97dc16653ba699
Congying Zhao <t3cozh00@students.oamk.fi>
2024-04-30 21:46:01 +0300

    #6_Functions and subfunctions

---
+++ #6_Functions and subfunctions/#6.15.1.py
@@ -0,0 +1,15 @@
+#15.1 Exercise: Basic subfunction
+def hello():
+    print('Hello there!')
+def main():
+    print('Lets call the subfunction:')
+    hello()
+    print('Quitting.')
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
